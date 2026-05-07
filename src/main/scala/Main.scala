dc8da978c49c323c2a88b143755b7af48b5f8e2b
Jordi <88168924+JordiCardona7@users.noreply.github.com>
2026-05-06 22:53:59 -0600

    subiendo proyecto scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,37 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.StdIn.readLine
+import scala.util.Random
 
-def msg = "I was compiled by Scala 3. :)"
+object AdivinarNumero {
+
+  // Función para verificar el número
+  def verificar(numeroSecreto: Int, intento: Int): String = {
+    if (intento < numeroSecreto) {
+      "Muy bajo"
+    } else if (intento > numeroSecreto) {
+      "Muy alto"
+    } else {
+      "Correcto"
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val numeroSecreto = Random.nextInt(10) + 1
+    var adivinado = false
+
+    println("Adivina el número (1-10)")
+
+    while (!adivinado) {
+      print("Ingresa tu número: ")
+      val intento = readLine().toInt
+
+      val resultado = verificar(numeroSecreto, intento)
+      println(resultado)
+
+      if (resultado == "Correcto") {
+        adivinado = true
+      }
+    }
+
+    println("¡Ganaste!")
+  }
+}
